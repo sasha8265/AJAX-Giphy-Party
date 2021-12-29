a3e8d85bc823bdfa69ff4a880ed15fc56fb1fc2e
Sasha Czerniawski <sashacandoit@gmail.com>
2021-12-29 07:01:35 -0500

    get random giphy function added

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 console.log("Let's get this party started!");
+
+
+
+async function getGiphys(q) {
+    const res = await axios.get('https://api.giphy.com/v1/gifs/search', { params: { q, api_key: 'IIH5XSkvZHZmaIuygDyWHMnxks62I1xo' } });
+    const resultIndex = Math.floor(Math.random() * 49);
+    console.log(res.data.data[resultIndex].embed_url);
+}
+
+getGiphys('cat');
